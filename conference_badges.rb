e21c1350ae8dc9c170de76525968cdf3f9ce9c38
Adam Johnson <adamwjo@gmail.com>
2018-07-23 19:23:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  x = batch_badge_creator(attendees)
+  y = assign_rooms(attendees)
 
 end
